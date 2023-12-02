b3e10422a006315623d3461d3694b9f7c519cecc
Andre Badan <badan.andre@gmail.com>
2023-12-02 18:07:46 -0300

    New

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');  // Adicionado o pacote cors
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,66 +11,58 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(session({ 
-  secret: 'secretpassword', 
-  resave: true, 
+app.use(session({
+  secret: 'secretpassword',
+  resave: true,
   saveUninitialized: true,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 dias de validade
 }));
+app.use(cors());  // Adicionado middleware CORS
 
-// Dados temporários para simular um banco de dados
 const usuarios = [
   { username: 'usuario', password: 'senha' }
 ];
 
-// Função auxiliar para verificar as credenciais
 function verificarCredenciais(username, password) {
   return usuarios.some(user => user.username === username && user.password === password);
 }
 
-// Rota para login
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // Verificar as credenciais
-    if (verificarCredenciais(username, password)) {
-        req.session.user = username;
+  if (verificarCredenciais(username, password)) {
+    req.session.user = username;
 
-        // Configurar cookies
-        const now = new Date();
-        const ultimoAcesso = now.toISOString();
-        res.cookie('ultimoAcesso', ultimoAcesso, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
-        res.cookie('nomeUsuario', username, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+    const now = new Date();
+    const ultimoAcesso = now.toISOString();
+    res.cookie('ultimoAcesso', ultimoAcesso, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+    res.cookie('nomeUsuario', username, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
 
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
 });
 
-// Rota para obter informações do usuário logado
 app.get('/infoUsuario', (req, res) => {
-    const nomeUsuario = req.session.user || '';
-    const ultimoAcesso = req.cookies.ultimoAcesso || '';
+  const nomeUsuario = req.session.user || '';
+  const ultimoAcesso = req.cookies.ultimoAcesso || '';
 
-    res.json({ nomeUsuario, ultimoAcesso });
+  res.json({ nomeUsuario, ultimoAcesso });
 });
 
 app.get('/dashboard', (req, res) => {
-    // Renderizar a página HTML do dashboard usando o caminho absoluto
-    res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+  res.sendFile(path.join(__dirname, '../public/dashboard.html'));
 });
 
-// Rota para obter dados da tabela
 app.get('/dadosTabela', (req, res) => {
-    // Obter dados do banco de dados ou outro armazenamento
-    const dados = [
-        { id: 1, nome: 'Exemplo 1' },
-        { id: 2, nome: 'Exemplo 2' },
-        // Outros dados da tabela
-    ];
+  const dados = [
+    { id: 1, nome: 'Exemplo 1' },
+    { id: 2, nome: 'Exemplo 2' },
+  ];
 
-    res.json(dados);
+  res.json(dados);
 });
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
